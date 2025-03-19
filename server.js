809a8418791c45e7460e77ee9dc5379a05fb9164
DussaUday <dussauday469@gmail.com>
2025-03-19 18:54:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+//import cors from "cors";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -23,11 +23,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); 
 app.use(cookieParser());
-app.use(cors({
-	   origin: ["http://localhost:3000"], // Your frontend URL
- 	   methods: ["POST", "GET"],// Your frontend URL
-	   credentials: true,
-	 }));
 
 
 
