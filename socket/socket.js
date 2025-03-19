84315438e0212bc60be915409ce61fa29386bd00
DussaUday <dussauday469@gmail.com>
2025-03-19 19:21:45 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -8,9 +8,13 @@ import User from "../models/user.model.js";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-	cors: {
+	cors: {[
 		origin: ["http://localhost:3000"],
+	       "https://socialty.vercel.app",            // Vercel frontend
+            "https://sociality-1.onrender.com"
+		],
 		methods: ["GET", "POST"],
+	       credentials: true,
 	},
 });
 
